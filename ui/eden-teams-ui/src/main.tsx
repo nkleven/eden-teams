6a0eb7e1c1ed760550dbca2f61117ca65bf81634
Nate Kleven <nkleven@hotmail.com>
2025-12-15 15:44:16 -0800

    feat: implement authentication flow with login page and routing

--- ui/eden-teams-ui/src/main.tsx
+++ ui/eden-teams-ui/src/main.tsx
@@ -1,10 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { PublicClientApplication } from "@azure/msal-browser";
+import { MsalProvider } from "@azure/msal-react";
+import { msalConfig } from "./auth/msalConfig";
+import App from "./App";
+import "./index.css";
 
-createRoot(document.getElementById('root')!).render(
+const msalInstance = new PublicClientApplication(msalConfig);
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <MsalProvider instance={msalInstance}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </MsalProvider>
+  </StrictMode>
+);
